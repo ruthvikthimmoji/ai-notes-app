7388a594381161206f8b7cfcc37ec9addb4f0707
Ruthviikthimmoji <149005776+ruthvikthimmoji@users.noreply.github.com>
2025-04-24 12:58:28 +0530

    added

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,6 @@
 
 'use client';
-
+export const dynamic = 'force-dynamic'
 import React, { useState } from 'react';
 import supabase from '@/lib/supabaseClient';
 import { useRouter } from 'next/navigation';
